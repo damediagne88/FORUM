5ab1ae672fdd5f0e1e8aadd6a57ea8da656198d4
El HADJI DAME DIAGNE <damediagnea@gmail.com>
2020-08-22 18:49:26 +0200

    Modification de mon fichier factory en la peuplent avec tinker

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,10 +3,15 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model;
+use App\Topic;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Topic::class, function (Faker $faker) {
     return [
-        //
+
+        'title'   => $faker->sentence,
+        'content' => $faker->paragraph,
+        'user_id' => factory('App\User')->create()
+
     ];
 });
